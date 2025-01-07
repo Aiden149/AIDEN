f897ea0e3e40a205d2b009a09a5dad8ffecf912e
Aiden149 <giftedfredrick16@gmail.com>
2025-01-07 22:51:09 +0300

    Update ping.js

--- Andbad_cmds/ping.js
+++ Andbad_cmds/ping.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🫀", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH 𝚫𝚴𝐃𝚩𝚫𝐃 MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
-    let d = '                                                                           I LOVE MY OWNER 𝚳𝚪 𝚫𝚴𝐃𝚩𝚫𝐃';
+    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH Aiden149 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
+    let d = '                                                                           I LOVE MY OWNER 𝐀𝐈𝐃𝐄𝐍_𝐓𝐄𝐂𝐇-𝐌𝐃';
     let varmess = z + d;
-    var video = 'https://i.imgur.com/UfpoNZT.mp4';
+    var video = 'https://files.catbox.moe/sb8niw.jpg';
     await zk.sendMessage(dest, { video: { url: img }, caption: varmess });
     //console.log("montest")
 });
